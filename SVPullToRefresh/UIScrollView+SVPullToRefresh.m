e345148b40da95a393ff8b85635f0cf9e51d8670
zhaosirui <zhaosirui@iautos.cn>
2017-10-22 00:12:14 +0800

    remove top + 64

--- SVPullToRefresh/UIScrollView+SVPullToRefresh.m
+++ SVPullToRefresh/UIScrollView+SVPullToRefresh.m
@@ -91,7 +91,7 @@ static char UIScrollViewPullToRefreshView;
     view.scrollView = self;
     [self addSubview:view];
     
-    view.originalTopInset = self.contentInset.top + 64;
+    view.originalTopInset = self.contentInset.top;
     view.originalBottomInset = self.contentInset.bottom;
     view.position = position;
     self.pullToRefreshView = view;
